d86850c07f155cac090e4093043fbb580cb2ff0e
miraexhoi <miraechoi@dgsw.hs.kr>
2024-08-12 23:56:26 +0900

    Initial Commit

---
+++ apple-foundation-program/apple_foundation_programApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_foundation_programApp.swift
+//  apple-foundation-program
+//
+//  Created by Mirae on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct apple_foundation_programApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
