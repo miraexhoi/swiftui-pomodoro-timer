b717262705c61ecafdef9c63a1f229c7cd8522bf
miraexhoi <miraechoi@dgsw.hs.kr>
2024-08-13 13:06:47 +0900

    feat :: timer properties

---
+++ apple-foundation-program/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  apple-foundation-program
+//
+//  Created by Mirae on 8/13/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
--- apple-foundation-program/ViewModel/PomodoroModel.swift
+++ apple-foundation-program/ViewModel/PomodoroModel.swift
@@ -9,4 +9,12 @@ import SwiftUI
 
 class PomodoroModel: NSObject, ObservableObject {
     // MARK : Timer Properties
+    @Published var progress: CGFloat = 1
+    @Published var timerStringValue: String = "00:00"
+    @Published var isStarted: Bool = false
+    @Published var addNewTimer: Bool = false
+    
+    @Published var hour: Int = 0
+    @Published var minutes: Int = 0
+    @Published var seconds: Int = 0
 }
